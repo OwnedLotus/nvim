0e13391f14ad17ead23f03d16ea7061f67aa95fc
Jonah Ringdahl <JonahRingdahl@outlook.com>
2025-09-26 17:39:33 -0400

    finish basic config

--- init.lua
+++ init.lua
@@ -1,14 +1,20 @@
+-- Set leader keys
+vim.g.mapleader = " " -- space as leader
+vim.g.maplocalleader = " " -- optional, for local leader
+
 -- init.lua
 
 -- Bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,3 +25,13 @@ require("lazy").setup("plugins")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.filetype.add({
+	extension = {
+		razor = "razor",
+		cshtml = "razor",
+	},
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,93 +1,206 @@
 -- lua/plugins.lua
 
 return {
-  -- Example colorscheme
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end
-  },
+	-- Example colorscheme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-  -- File explorer
-  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup()
-    end
-  },
+	-- Statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	-- Telescope (fuzzy finder)
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					layout_config = { prompt_position = "top" },
+					sorting_strategy = "ascending",
+					winblend = 10,
+				},
+			})
 
-  -- Statusline
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup()
-    end
-  },
+			-- Keymaps
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+		end,
+	},
 
-  -- Mason
-  --{ "williamboman/mason.nvim",
-   -- config = function()
-    --  require("mason").setup()
-    --end
-  --},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Bridge
-  { "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-	require("mason").setup()
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"basedpyright",
+					"ts_ls",
+					"rust_analyzer",
+					"clangd",
+					"zls",
+					"ols",
+				}, -- auto-install these
+				automatic_installation = true,
+			})
+		end,
+	},
 
-      local mason_lspconfig = require("mason-lspconfig")
-      mason_lspconfig.setup {
-        ensure_installed = { "lua_ls", "basedpyright", "csharp_ls", "zls", "ols", "clangd", "rust_analyzer", "ts_ls" }, -- auto-install these
-      	}
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup()
+		end,
+	},
 
-	  -- Define default on_attach and capabilities
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    local on_attach = function(_, bufnr)
-      local opts = { buffer = bufnr, silent = true }
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    end
+	-- Formatter
+	-- Mason bridge for formatters/linters
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier", -- JS/TS, CSS, HTML
+					"stylua", -- Lua
+					"black", -- Python
+					"isort", -- Python imports
+					"csharpier", -- C# formatter
+					"clang-format", -- C++ formatter
+					"cmakelang", -- CMAKE formatter
+				},
+				auto_update = true,
+			})
+		end,
+	},
 
-    -- Setup handlers for each server
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        vim.lsp.config[server_name] = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        }
-        -- Automatically start the server when entering a matching buffer
-        vim.api.nvim_create_autocmd("FileType", {
-          pattern = server_name, -- works for most servers
-          callback = function(args)
-            vim.lsp.start(vim.lsp.config[server_name], { bufnr = args.buf })
-          end,
-        })
-      end
-      }
+	-- Formatter plugin
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					json = { "prettier" },
+					html = { "prettier" },
+					css = { "prettier" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				},
+			})
+		end,
+	},
+	-- Core DAP plugin
+	{ "mfussenegger/nvim-dap" },
 
-        },
+	-- Mason bridge for DAP
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "python", "codelldb", ".NET" }, -- adapters to auto-install
+				automatic_installation = true,
+			})
 
-  -- Telescope (fuzzy finder)
-  { "nvim-telescope/telescope.nvim", tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup {
-        defaults = {
-          layout_config = { prompt_position = "top" },
-          sorting_strategy = "ascending",
-          winblend = 10,
-        },
-      }
-      -- Keymaps
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Find buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Help tags" })
-    end
-  },
+			local dap = require("dap")
+
+			-- Basic debugging
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP Continue/Start" })
+			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
+			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
+			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+
+			-- Breakpoints
+			vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+			vim.keymap.set("n", "<Leader>B", function()
+				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end, { desc = "DAP Conditional Breakpoint" })
+
+			-- REPL & UI
+			vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "DAP Open REPL" })
+			vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "DAP Run Last" })
+
+			-- If using nvim-dap-ui
+			local dapui = require("dapui")
+			vim.keymap.set("n", "<Leader>du", dapui.toggle, { desc = "DAP UI Toggle" })
+		end,
+	},
+
+	-- DAP UI
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+
+			-- Auto open/close UI
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+
+	-- Csharp ls
+	{
+		"seblj/roslyn.nvim",
+		config = function()
+			on_attach = function(client, bufnr)
+				--Keymap
+			end
+		end,
+	},
+
+	{
+		"tris203/rzls.nvim",
+		config = function()
+			on_attach = function(client, bufnr)
+				--Keymap
+			end
+		end,
+	},
+
+	-- Snippets (for Razor/C#)
+	{ "hrsh7th/vim-vsnip" },
+	{ "OrangeT/vim-csharp" },
 }
