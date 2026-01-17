b2fd6454f0d37654758e25ef764f7af46cff74c5
Jonah Ringdahl <JonahRingdahl@outlook.com>
2026-01-17 12:02:42 -0500

    dont force cmake when not working

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,29 +1,29 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "html",
-                "cssls",
-                "ts_ls",
-                "ols",
-                "zls",
-                "clangd",
-                "cmake",
-                "lua_ls",
-            },
-            automatic_installation = true,
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = require('blink.cmp').get_lsp_capabilities(),
-                        on_attach = function(client, bufnr)
-                            -- LSP keybindings are handled in lua/config/lsp.lua
-                        end,
-                    })
-                end,
-            },
-        })
-    end,
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "html",
+        "cssls",
+        "ts_ls",
+        "ols",
+        "zls",
+        "clangd",
+        "lua_ls",
+      },
+      automatic_installation = true,
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = require('blink.cmp').get_lsp_capabilities(),
+            on_attach = function(client, bufnr)
+              -- LSP keybindings are handled in lua/config/lsp.lua
+            end,
+          })
+        end,
+      },
+    })
+  end,
 }
+
